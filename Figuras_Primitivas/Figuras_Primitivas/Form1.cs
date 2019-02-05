fa93bbbcbd7a50c99aa6d128f4866091f36b5798
guillermosantos2909 <47370985+guillermosantos2909@users.noreply.github.com>
2019-02-05 14:50:14 -0800

    Add files via upload

---
+++ Figuras_Primitivas/Figuras_Primitivas/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Figuras_Primitivas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics obj = CreateGraphics();
+            Pen pen = new Pen(Color.Aqua, 8);
+            obj.DrawLine(pen, 100, 150, 200, 200);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Graphics obj = this.CreateGraphics();
+            Pen pen = new Pen(Color.BlueViolet, 5);
+            obj.DrawRectangle(pen, 600, 150, 100, 100);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Graphics obj = this.CreateGraphics();
+            Pen pen = new Pen(Color.Green, 3);
+            obj.DrawEllipse(pen, 350, 150, 100, 100);
+        }
+    }
+}
